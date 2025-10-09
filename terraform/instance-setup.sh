a30ae44edf8b60e9de55cfedb05bbfb214e3d2ae
diana <dianamariamartisca@gmail.com>
2025-10-10 00:30:11 +0300

    secret env + workflows

--- terraform/instance-setup.sh
+++ terraform/instance-setup.sh
@@ -8,12 +8,10 @@ BRANCH="main"
 
 echo "Starting instance1 setup"
 
-# Update and install Docker and Git
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y docker.io git
 
-# Install Docker Compose
 DOCKER_COMPOSE_VERSION="1.29.2"
 curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -22,7 +20,6 @@ chmod +x /usr/local/bin/docker-compose
 mkdir -p ${APP_DIR}
 chown -R ${USER}:${USER} ${APP_DIR} || true
 
-# Clone the repository
 if [ ! -d "${APP_DIR}/.git" ]; then
   git clone --branch ${BRANCH} ${GIT_REPO} ${APP_DIR}
 else
@@ -33,5 +30,4 @@ fi
 
 cd ${APP_DIR}
 
-# Start the application using Docker Compose
 docker-compose up -d
