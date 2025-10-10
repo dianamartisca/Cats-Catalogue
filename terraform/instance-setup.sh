134e5ea995f39e20f8b3a0770e28813e408d386b
diana <dianamariamartisca@gmail.com>
2025-10-10 16:28:29 +0300

    removed comments

--- terraform/instance-setup.sh
+++ terraform/instance-setup.sh
@@ -16,7 +16,6 @@ DOCKER_COMPOSE_VERSION="1.29.2"
 curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Create application directory
 mkdir -p ${APP_DIR}
 chown -R ${USER}:${USER} ${APP_DIR} || true
 
